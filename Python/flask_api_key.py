db0a29abc923ba261de9296bb510cf10ec2d863d
Andrew Morris <morr.drew@gmail.com>
2016-11-20 00:18:11 -0500

    added flask api key example

---
+++ Python/flask_api_key.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+from flask import Flask, request, abort
+from functools import wraps
+from werkzeug.wrappers import Response
+
+app = Flask(__name__)
+
+def some_validation_function(apikey):
+    apikeys = ["AAAA", "BBBB", "CCCC"]
+    return apikey in apikeys
+
+def require_appkey(view_function):
+    @wraps(view_function)
+    def decorated_function(*args, **kwargs):
+        if request.args.get('apikey') and some_validation_function(request.args.get('apikey')):
+            return view_function(*args, **kwargs)
+        else:
+            abort(401)
+    return decorated_function
+
+@app.route('/')
+def unprotected():
+    return 'HAYYY'
+
+@app.route('/protected', methods=["POST","GET"])
+@require_appkey
+def protected():
+    return 'SOMETHING SECRET!!!'
+
+@app.errorhandler(401)
+def custom_401(error):
+    return Response('Invalid API Key. Please contact someone@shittystartup.io', 401, {'WWWAuthenticate':'Basic realm="Login Required"'}) 
+
